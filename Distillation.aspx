16ef3a30e19707e784209b37add358c0129e9d7b
sienergy <evdatamatrix@gmail.com>
2026-04-03 12:36:13 +0530

    final raw

--- Distillation.aspx
+++ Distillation.aspx
@@ -2,4 +2,34 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <h2>Distillation Report</h2>
+
+    <table>
+        <tr>
+            <td>Start Date:</td>
+            <td>
+                <asp:TextBox ID="txtStartDate" runat="server" TextMode="DateTimeLocal"></asp:TextBox>
+            </td>
+        </tr>
+
+        <tr>
+            <td>End Date:</td>
+            <td>
+                <asp:TextBox ID="txtEndDate" runat="server" TextMode="DateTimeLocal"></asp:TextBox>
+            </td>
+        </tr>
+
+        <tr>
+            <td colspan="2">
+                <asp:Button ID="btnShow" runat="server" Text="Show Report" OnClick="btnShow_Click" />
+            </td>
+        </tr>
+    </table>
+
+    <br />
+
+    <asp:GridView ID="gvReport" runat="server" AutoGenerateColumns="true" 
+        CssClass="table table-bordered" />
+
 </asp:Content>
--- Distillation.aspx.cs
+++ Distillation.aspx.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace PRAJ_Report
 {
@@ -11,7 +9,57 @@ namespace PRAJ_Report
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                // Optional: Set default dates
+                txtStartDate.Text = DateTime.Now.AddHours(-1).ToString("yyyy-MM-ddTHH:mm");
+                txtEndDate.Text = DateTime.Now.ToString("yyyy-MM-ddTHH:mm");
+            }
+        }
 
+        protected void btnShow_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get values from UI
+                DateTime startDate = Convert.ToDateTime(txtStartDate.Text);
+                DateTime endDate = Convert.ToDateTime(txtEndDate.Text);
+
+                // Get from Query String (URL)
+                string dbName = Request.QueryString["db"];
+                string typeStr = Request.QueryString["type"];
+
+                int type = 0;
+                if (!string.IsNullOrEmpty(typeStr))
+                    type = Convert.ToInt32(typeStr);
+
+                // Connection string from Web.config
+                string connStr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+
+                using (SqlConnection con = new SqlConnection(connStr))
+                {
+                    using (SqlCommand cmd = new SqlCommand("GetTagData", con))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        cmd.Parameters.AddWithValue("@StartDate", startDate);
+                        cmd.Parameters.AddWithValue("@EndDate", endDate);
+                        cmd.Parameters.AddWithValue("@DBName", dbName);
+                        cmd.Parameters.AddWithValue("@Type", type);
+
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+
+                        gvReport.DataSource = dt;
+                        gvReport.DataBind();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "')</script>");
+            }
         }
     }
 }
